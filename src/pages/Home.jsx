f5258265e7ae4efefd4ee38b5164b7e607218e4f
Puyan Wei <puyanwei@gmail.com>
2019-10-12 18:27:49 +0100

    Added useRef to keep track of input value, tested wtih onChange function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './home.scss';
 
 const Home = () => {
+	const inputValue = useRef('');
+
+	const logInput = () => {
+		console.log(inputValue.current.value);
+	};
+
 	return (
 		<div className="homepage-container">
 			<h1>Company Finder</h1>
+
 			<form>
-				<input type="text" />
+				<input type="text" placeholder="Enter GUID here" ref={inputValue} onChange={logInput} />
 				<button type="submit">Fetch Company Details!</button>
 			</form>
 		</div>
