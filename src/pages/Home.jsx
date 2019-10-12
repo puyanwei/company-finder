16de9a42864041f93b082afb7919ba5be4d1700b
Puyan Wei <puyanwei@gmail.com>
2019-10-12 18:20:21 +0100

    Added Sass. Newer React version does not require compiler!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './home.scss';
 
 const Home = () => {
 	return (
-		<div>
+		<div className="homepage-container">
 			<h1>This is the Homepage</h1>
 		</div>
 	);
