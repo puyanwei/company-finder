e3890f7a92f8ba74158ef4e5dca00a98c92348bc
Puyan Wei <puyanwei@gmail.com>
2019-10-12 18:32:31 +0100

    Added handleSubmit function, removed test onChange function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,17 @@ import './home.scss';
 const Home = () => {
 	const inputValue = useRef('');
 
-	const logInput = () => {
-		console.log(inputValue.current.value);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('submits');
 	};
 
 	return (
 		<div className="homepage-container">
 			<h1>Company Finder</h1>
 
-			<form>
-				<input type="text" placeholder="Enter GUID here" ref={inputValue} onChange={logInput} />
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<input type="text" placeholder="Enter GUID here" ref={inputValue} />
 				<button type="submit">Fetch Company Details!</button>
 			</form>
 		</div>
