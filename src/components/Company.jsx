b8678acce4f2fc239240fc03a7b49d3bf73a6979
Puyan Wei <puyanwei@gmail.com>
2019-10-13 14:15:46 +0100

    Fix for adding unique keys to list

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -5,14 +5,14 @@ import './company.scss';
 const Company = ({ companyData }) => {
 	const [ isHidden, setIsHidden ] = useState(true);
 
-	const { Name, Abn, Score } = companyData;
+	const { Name, Abn } = companyData;
 	return (
 		<li className="company-title" onClick={() => setIsHidden(!isHidden)}>
 			{Name} {Abn}
 			{!isHidden ? (
 				<ul>
 					{Object.keys(companyData).map((key) => (
-						<li className="company-details">
+						<li className="company-details" key={`${key} ${companyData[key]}`}>
 							{key}: {companyData[key]}
 						</li>
 					))}
