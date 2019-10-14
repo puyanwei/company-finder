a939781f23e0621c7f2cf59d9f2de4e2c173f887
Puyan Wei <puyanwei@gmail.com>
2019-10-14 23:44:02 +0100

    Updates

---
+++ precache-manifest.8d0f49d8c2c1f3c8fbe650686e451ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e2652ebfe3293224b4209c2d9f5d60",
+    "url": "/company-finder/index.html"
+  },
+  {
+    "revision": "c16db6d21861c014d3bd",
+    "url": "/company-finder/static/css/main.da536f48.chunk.css"
+  },
+  {
+    "revision": "63b497e041ecd11c2f27",
+    "url": "/company-finder/static/js/2.f4d0b975.chunk.js"
+  },
+  {
+    "revision": "c16db6d21861c014d3bd",
+    "url": "/company-finder/static/js/main.92d5a1e5.chunk.js"
+  },
+  {
+    "revision": "812dff77aa7dda5db2d9",
+    "url": "/company-finder/static/js/runtime-main.8d7c6cf9.js"
+  }
+]);
