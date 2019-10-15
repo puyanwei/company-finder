6b323716d078713105c907a55ef02dd81f179a92
Puyan Wei <puyanwei@gmail.com>
2019-10-15 01:00:36 +0100

    Fixed another prop type error

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -4,32 +4,32 @@ import PropTypes from 'prop-types';
 import './company.scss';
 
 const Company = ({ companyData }) => {
-	const [ isHidden, setIsHidden ] = useState(true);
+    const [isHidden, setIsHidden] = useState(true);
 
-	const ignoreParentClick = (e) => {
-		e.stopPropagation();
-	};
+    const ignoreParentClick = (e) => {
+        e.stopPropagation();
+    };
 
-	return (
-		<li className="company-title" onClick={() => setIsHidden(!isHidden)}>
-			{companyData.Name ? companyData.Name : companyData.EntityName}
-			<span className="toggle-icon">{isHidden ? '+' : '-'}</span>
+    return (
+        <li className="company-title" onClick={() => setIsHidden(!isHidden)}>
+            {companyData.Name ? companyData.Name : companyData.EntityName}
+            <span className="toggle-icon">{isHidden ? '+' : '-'}</span>
 
-			{!isHidden ? (
-				<ul onClick={(e) => ignoreParentClick(e)}>
-					{Object.keys(companyData).map((key) => (
-						<li className="company-details" key={`${key} ${companyData[key]}`}>
-							{key}: {companyData[key]}
-						</li>
-					))}
-				</ul>
-			) : null}
-		</li>
-	);
+            {!isHidden ? (
+                <ul onClick={(e) => ignoreParentClick(e)}>
+                    {Object.keys(companyData).map((key) => (
+                        <li className="company-details" key={`${key} ${companyData[key]}`}>
+                            {key}: {companyData[key]}
+                        </li>
+                    ))}
+                </ul>
+            ) : null}
+        </li>
+    );
 };
 
 Company.propTypes = {
-	companyData: PropTypes.objectOf(PropTypes.string).isRequired
+    companyData: PropTypes.objectOf(PropTypes.any).isRequired
 };
 
 export default Company;
