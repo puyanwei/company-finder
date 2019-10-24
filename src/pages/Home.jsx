9b30e594c97024c9296ea2027ddf2c1b84bd19c5
Puyan Wei <puyanwei@gmail.com>
2019-10-24 23:31:25 +0100

    Fixing linting on Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,67 +5,67 @@ import ResultsPanel from '../components/ResultsPanel';
 import './home.scss';
 
 const Home = () => {
-    const [ formInput, setFormInput ] = useState({ guid: "", query: "" });
-    const [ state, setState ] = useState({ isError:false, isLoading: false });
-    const [ formData, setFormData ] = useState(null);
+    const [formInput, setFormInput] = useState({ guid: "", query: "" });
+    const [state, setState] = useState({ isError: false, isLoading: false });
+    const [formData, setFormData] = useState(null);
 
     const { isError, isLoading } = state
     const { guid, query } = formInput
 
     const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log('submitting...');
-		setState({ ...state, isLoading: true });
-		nameOrABNQueryURL();
+        e.preventDefault();
+        console.log('submitting...');
+        setState({ ...state, isLoading: true });
+        nameOrABNQueryURL();
     };
-    
+
     const handleKeyPress = (e) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         name === "guid"
             ?
             setFormInput({ ...formInput, guid: value })
             : setFormInput({ ...formInput, query: value })
     }
 
-	const nameOrABNQueryURL = () => {
-		let removeString;
-		let url;
+    const nameOrABNQueryURL = () => {
+        let removeString;
+        let url;
 
-		if (typeof Number(query) === 'number' && query.length === 11) {
-			url = `AbnDetails.aspx?callback=callback&abn=${query}&guid=${guid}`;
-			removeString = 'callback(';
-		} else {
-			url = `MatchingNames.aspx?callback=nameCallback&name=${query}&guid=${guid}`;
-			removeString = 'nameCallback(';
-		}
-		fetchData(url, removeString);
-	};
+        if (typeof Number(query) === 'number' && query.length === 11) {
+            url = `AbnDetails.aspx?callback=callback&abn=${query}&guid=${guid}`;
+            removeString = 'callback(';
+        } else {
+            url = `MatchingNames.aspx?callback=nameCallback&name=${query}&guid=${guid}`;
+            removeString = 'nameCallback(';
+        }
+        fetchData(url, removeString);
+    };
 
-	const fetchData = (url, removeString) => {
-		const myHeaders = new Headers();
-		myHeaders.append('Content-Type', 'text/plain; charset=ISO-8859-1');
+    const fetchData = (url, removeString) => {
+        const myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'text/plain; charset=ISO-8859-1');
 
-		fetch(`https://abr.business.gov.au/json/${url}`)
-			.then((response) => {
-				return response.text();
-			})
-			.then((result) => {
-				let hash = result.replace(removeString, '').slice(0, -1);
-				hash = JSON.parse(hash);
-				removeString === 'nameCallback(' ? setFormData(hash.Names) : setFormData([ hash ]);
-			})
-			.catch((err) => {
+        fetch(`https://abr.business.gov.au/json/${url}`)
+            .then((response) => {
+                return response.text();
+            })
+            .then((result) => {
+                let hash = result.replace(removeString, '').slice(0, -1);
+                hash = JSON.parse(hash);
+                removeString === 'nameCallback(' ? setFormData(hash.Names) : setFormData([hash]);
+            })
+            .catch((err) => {
                 console.log(err);
                 setState({ isLoading: false, isError: true })
-			})
-			.finally(() => {
+            })
+            .finally(() => {
                 setState({ isLoading: false, ...state })
-			});
-	};
+            });
+    };
 
-	const createUniqueKey = (company) => `${company.EntityName}${company.Abn}`;
+    const createUniqueKey = (company) => `${company.EntityName}${company.Abn}`;
 
-	return (
+    return (
         <>
             <div className="homepage-container">
                 <h1 className="homepage-title">ABN LOOKUP</h1>
@@ -78,12 +78,12 @@ const Home = () => {
             </div>
             <>
                 {isLoading
-                    ? 
+                    ?
                     <h2>LOADING...</h2>
-                    : <ResultsPanel data={formData} queryValue={{query}} createUniqueKey={createUniqueKey} />}
+                    : <ResultsPanel data={formData} queryValue={query} createUniqueKey={createUniqueKey} />}
             </>
         </>
-	);
+    );
 };
 
 export default Home;
